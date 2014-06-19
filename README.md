c426c96a3cb8a816396d6b5867495e24a26c09ae
Manuel Lehner <manumaticx@gmail.com>
2014-06-19 17:14:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# PagingControl [![Titanium](http://www-static.appcelerator.com/badges/titanium-git-badge-sq.png)](http://www.appcelerator.com/titanium/) [![Appcelerator Titanium](http://www-static.appcelerator.com/badges/alloy-git-badge-sq.png)](http://www.appcelerator.com/alloy/)
+
+Alloy Widget for a line page indication on ScrollableViews.
+
+![](demo.gif)
+
+## Quick Start
+
+### Installation [![gitTio](http://gitt.io/badge.png)](http://gitt.io/component/de.manumaticx.pagingcontrol)
+Download the latest distribution ZIP-file and consult the [Titanium Documentation](http://docs.appcelerator.com/titanium/latest/#!/guide/Using_a_Module) on how install it, or simply use the [gitTio CLI](http://gitt.io/cli):
+
+`$ gittio install de.manumaticx.pagingcontrol`
+
+### Usage
+Just put it in the same container as the ScrollableView:
+```xml
+<Alloy>
+	<Window class="container" >
+		<ScrollableView id="scrollableView">
+		    <ImageView image="/images/image01.jpg" />
+		    <ImageView image="/images/image02.jpg" />
+		    <ImageView image="/images/image03.jpg" />
+		    <ImageView image="/images/image04.jpg" />
+		</ScrollableView>
+		<Widget id="paging" src="de.manumaticx.pagingcontrol" />
+	</Window>
+</Alloy>
+```
+You can also pass a reference to the ScrollableView as `scrollableView` at creation or later with `$.paging.setScrollableView($.scrollableView)`. But as long as the pagingControl widget and the ScrollableView have the same parent view, it should initialize itself.
+
+Use `indicatorColor` to define the color.
+
+## License
+
+    The MIT License (MIT)
+
+    Copyright (c) 2014 Manuel Lehner
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
---
+++ de.manumaticx.pagincontrol/controllers/widget.js
@@ -0,0 +1,100 @@
+var args = _.defaults(arguments[0] || {}, {
+    indicatorColor: "#000",
+    height: 5,
+    width: Ti.UI.FILL,
+    bottom: 0
+});
+
+// apply properties of Ti.UI.View that can be applied to paging control view
+[
+    "backgroundColor",
+    "backgroundImage",
+    "backgroundLeftCap",
+    "backgroundRepeat",
+    "backgroundTopCap",
+    "borderRadius",
+    "borderWidth",
+    "bottom",
+    "height",
+    "horizontalWrap",
+    "left",
+    "opacity",
+    "right",
+    "top",
+    "visible",
+    "width",
+    "zIndex"
+].forEach(function(prop){ _.has(args, prop) && ($.pagingcontrol[prop] = args[prop]); });
+
+// try to find scrollable view as child of parent
+// this should work, if pagingcontrol is scrollable view have the same parent
+// otherwise you can pass it with args or setScrollableView
+if (args.__parentSymbol){
+    args.__parentSymbol.children.length > 0 && 
+    ($.scrollableView = _.find(args.__parentSymbol.children, function(child){
+        return child.apiName === "Ti.UI.ScrollableView";
+    }));
+}
+
+// assign passed reference of scrollable view
+(_.has(args, "scrollableView")) && ($.scrollableView = args.scrollableView);
+
+if ($.scrollableView){
+    initWhenReady();
+    Ti.API.info("init called at creation");
+}
+
+/**
+ * calls init if we have all infos
+ */
+function initWhenReady(){
+    // wait for postlayout event to get the pagingcontrol size 
+    $.pagingcontrol.addEventListener('postlayout', function onPostLayout(){
+        
+        // if we have a scrollableView reference, initialize the paging control
+        init();
+        
+        // remove eventlistener
+        $.pagingcontrol.removeEventListener('postlayout', onPostLayout);
+    });
+}
+
+/**
+ * initialization method
+ */
+function init(){
+    
+    // save the indicator width as it's used in scroll event
+    $.iWidth = Math.floor($.pagingcontrol.size.width / $.scrollableView.views.length);
+    
+    // create the indicator view
+    $.indicator = Ti.UI.createView({
+        backgroundColor: args.indicatorColor,
+        height: $.pagingcontrol.getHeight(),
+        width: $.iWidth,
+        left: 0
+    });
+    
+    // add the indicator
+    $.pagingcontrol.add($.indicator);
+    
+    // add scroll listener to scrollable view
+    $.scrollableView.addEventListener('scroll', onScroll);
+}
+
+/**
+ * Callback for scroll event
+ */
+function onScroll(e){
+    // here is where the magic happens, as simple as that
+    $.indicator.setLeft(e.currentPageAsFloat * $.iWidth);    
+}
+
+/**
+ * if you need to set it in the controller
+ * @param {Ti.UI.Scrollableview} scrollable view
+ */
+exports.setScrollableView = function(_sv){
+    $.scrollableView = _sv;
+    initWhenReady();
+};
